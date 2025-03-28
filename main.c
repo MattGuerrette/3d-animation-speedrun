2dda6e65393b923a0d7225b345006abc99a3f0fe
Mick Sayson <mick@sayson.com>
2025-03-28 13:33:21 -0700

    3d

--- main.c
+++ main.c
@@ -12,26 +12,26 @@
 
 typedef struct Vertex
 {
-    float pos[2];
+    float pos[3];
     float col[3];
 } Vertex;
 
 static const Vertex vertices[3] =
 {
-    { { -0.6f, -0.4f }, { 1.f, 0.f, 0.f } },
-    { {  0.6f, -0.4f }, { 0.f, 1.f, 0.f } },
-    { {   0.f,  0.6f }, { 0.f, 0.f, 1.f } }
+    { { -0.6f, -0.4f, 0.0f }, { 1.f, 0.f, 0.f } },
+    { {  0.6f, -0.4f, 0.0f }, { 0.f, 1.f, 0.f } },
+    { {   0.f,  0.6f, 0.0f }, { 0.f, 0.f, 1.f } }
 };
 
 static const char* vertex_shader_text =
 "#version 460\n"
 "layout(location = 0) in vec3 vCol;\n"
-"layout(location = 1) in vec2 vPos;\n"
-"layout(location = 0) uniform mat3 txfm;\n"
+"layout(location = 1) in vec3 vPos;\n"
+"layout(location = 0) uniform mat4 txfm;\n"
 "out vec3 color;\n"
 "void main()\n"
 "{\n"
-"    gl_Position = vec4((txfm * vec3(vPos, 1.0)).xy, 0.0, 1.0);\n"
+"    gl_Position = txfm * vec4(vPos, 1.0);\n"
 "    color = vCol;\n"
 "}\n";
 
@@ -45,69 +45,74 @@ static const char* fragment_shader_text =
 "}\n";
 
 
-struct mat3x3 {
-    float data[9];
+struct mat4x4 {
+    float data[16];
 };
 
-struct mat3x3 mat3x3_rot(float angle) {
+struct mat4x4 mat4x4_rot_z(float angle) {
     float c = cos(angle);
     float s = sin(angle);
 
-    return (struct mat3x3) {
-          c,   -s, 0.0,
-          s,    c, 0.0,
-        0.0,  0.0, 1.0,
+    return (struct mat4x4) {
+          c,   -s, 0.0, 0.0,
+          s,    c, 0.0, 0.0,
+        0.0,  0.0, 1.0, 0.0,
+        0.0,  0.0, 0.0, 1.0,
     };
 }
 
-struct mat3x3 mat3x3_translate(float x, float y) {
-    return (struct mat3x3) {
-        1.0,  0.0,   x,
-        0.0,  1.0,   y,
-        0.0,  0.0, 1.0,
+struct mat4x4 mat4x4_translate(float x, float y, float z) {
+    return (struct mat4x4) {
+        1.0,  0.0, 0.0,   x,
+        0.0,  1.0, 0.0,   y,
+        0.0,  0.0, 1.0,   z,
+        0.0,  0.0, 0.0, 1.0,
     };
 }
 
-struct vec3 {
-    float data[3];
+struct vec4 {
+    float data[4];
 };
 
-struct vec3 mat3x3_row(struct mat3x3 mat, int row) {
-    struct vec3 ret;
+struct vec4 mat4x4_row(struct mat4x4 mat, int row) {
+    struct vec4 ret;
 
-    int start_idx = row * 3;
+    int start_idx = row * 4;
     ret.data[0] = mat.data[start_idx];
     ret.data[1] = mat.data[start_idx + 1];
     ret.data[2] = mat.data[start_idx + 2];
+    ret.data[3] = mat.data[start_idx + 3];
     return ret;
 }
 
-struct vec3 mat3x3_col(struct mat3x3 mat, int col) {
-    struct vec3 ret;
+struct vec4 mat4x4_col(struct mat4x4 mat, int col) {
+    struct vec4 ret;
 
     ret.data[0] = mat.data[col];
-    ret.data[1] = mat.data[3 + col];
-    ret.data[2] = mat.data[6 + col];
+    ret.data[1] = mat.data[4 + col];
+    ret.data[2] = mat.data[8 + col];
+    ret.data[3] = mat.data[12 + col];
     return ret;
 }
 
-float vec3_dot(struct vec3 a, struct vec3 b) {
+float vec4_dot(struct vec4 a, struct vec4 b) {
     return a.data[0] * b.data[0] +
            a.data[1] * b.data[1] +
-           a.data[2] * b.data[2];
+           a.data[2] * b.data[2] +
+           a.data[3] * b.data[3];
 }
 
-struct mat3x3 mat3x3_mul(struct mat3x3 a, struct mat3x3 b) {
-    struct mat3x3 ret;
+struct mat4x4 mat4x4_mul(struct mat4x4 a, struct mat4x4 b) {
+    struct mat4x4 ret;
 
-    for (int i = 0; i < 9; ++i) {
-        int row = i / 3;
-        int col = i % 3;
+    for (int i = 0; i < 16; ++i) {
+        int row = i / 4;
+        int col = i % 4;
 
-        struct vec3 a_row = mat3x3_row(a, row);
-        struct vec3 b_col = mat3x3_col(b, col);
+        struct vec4 a_row = mat4x4_row(a, row);
+        struct vec4 b_col = mat4x4_col(b, col);
 
-        ret.data[i] = vec3_dot(a_row, b_col);
+        ret.data[i] = vec4_dot(a_row, b_col);
     }
 
     return ret;
@@ -199,9 +204,9 @@ int main(void)
         glUseProgram(program);
         glBindVertexArray(vertex_array);
 
-        struct mat3x3 txfm = mat3x3_translate(0, 1.0);
-        txfm = mat3x3_mul(txfm, mat3x3_rot(angle));
-        glUniformMatrix3fv(0, 1, true, txfm.data);
+        struct mat4x4 txfm = mat4x4_translate(0, 1.0, 0.0);
+        txfm = mat4x4_mul(mat4x4_rot_z(angle), txfm);
+        glUniformMatrix4fv(0, 1, true, txfm.data);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
